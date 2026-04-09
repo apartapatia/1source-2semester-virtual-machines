c4a7251f9e5ba6ce47fed366cc9f0fcfd8160102
apartapatia <raupov.deal@yandex.ru>
2026-04-09 21:50:08 +0300

    add vm username to output

--- infra/modules/compute/outputs.tf
+++ infra/modules/compute/outputs.tf
@@ -1,3 +1,7 @@
 output "public_ip" {
   value = yandex_compute_instance.this.network_interface[0].nat_ip_address
 }
+
+output "default_username" {
+  value = local.default_username
+}
--- infra/outputs.tf
+++ infra/outputs.tf
@@ -2,3 +2,8 @@ output "vm_public_ip" {
   description = "Public IP address of the VM"
   value       = module.compute.public_ip
 }
+
+output "vm_username" {
+  description = "VM username"
+  value       = module.compute.default_username
+}
